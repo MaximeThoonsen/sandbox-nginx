33f144747b8a91abb5d05e7d3a4317c8436ba665
Maxime Thoonsen <maximet@theodo.fr>
2014-08-28 11:20:55 +0100

    change ssl options

--- www/php/index.php
+++ www/php/index.php
@@ -9,5 +9,21 @@
 <p>
     <?php echo "Hello I am a echo php"; ?>
 </p>
+<pre>
+    server {
+        listen 80;
+        server_name nginx-php.demo;
+        root        /var/www/php/;
+
+        location ~ \.php {
+            fastcgi_index index.php;
+            fastcgi_pass   unix:/var/run/php5-fpm.sock;
+            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            include fastcgi_params;
+        }
+
+}
+
+</pre>
 </body>
 </html>
